dd46871ffbfb0c40f2e5f039d6bcb653267b7ac9
Yuriy Syrovetskiy <cblp@cblp.su>
2017-12-29 16:29:29 +0300

    Style

--- test.hs
+++ test.hs
@@ -17,7 +17,7 @@ import           Data.Char (isSpace)
 import           Data.String.Interpolate.IsString (i)
 import qualified Data.Text as Text
 import           Data.Yaml as Yaml
-import           Filesystem.Path.CurrentOS (encodeString, replaceExtension)
+import           Filesystem.Path.CurrentOS (encodeString)
 
 data Language = NoLanguage | C
     deriving Show
@@ -36,7 +36,7 @@ instance FromJSON SnippetConfig where
     parseJSON _ = fail "Expected object for SnippetConfig"
 
 defaultSnippetConfig :: SnippetConfig
-defaultSnippetConfig = SnippetConfig {after = "", include = []}
+defaultSnippetConfig = SnippetConfig{after = "", include = []}
 
 data Snippet = Snippet
     { config    :: SnippetConfig
@@ -50,79 +50,69 @@ data Snippet = Snippet
 main :: IO ()
 main = do
     files <- Turtle.sort $ ls "."
-    for_ files $ \file -> case extension file of
-        Just "md" -> do
-            putStr $ encodeString file <> " ...\t"
-            checkSnippets file
-            putText "OK"
-        _ -> pure () -- ignore
+    for_ files $ \file ->
+        case extension file of
+            Just "md" -> do
+                putText $ pathToS file <> " ...\t"
+                checkSnippets =<< realpath file
+            _ -> pure () -- ignore
     putText "OK"
 
 checkSnippets :: FilePath -> IO ()
-checkSnippets file = do
-    snippets <- extract file
-    for_ snippets checkSnippet
+checkSnippets = extract >=> traverse_ checkSnippet
 
 extract :: FilePath -> IO [Snippet]
-extract file = do
-    fileContents <- readTextFile file
-    pure $ map parseSnippet $ splitSnippets $ zip [1 ..] $ Text.lines
-        fileContents
+extract file =
+    map parseSnippet . splitSnippets . zip [1 ..] . Text.lines
+    <$> readTextFile file
   where
 
-    splitSnippets []        = []
+    splitSnippets [] = []
     splitSnippets fileLines = fromMaybe [] $ do
-        (_text, (lineNo, spec):afterHeader) <- pure $ breakSnippet fileLines
-        languageSpec                        <- Text.stripPrefix "```" spec
+        (_text, (lineNo, spec) : afterHeader) <- pure $ breakSnippet fileLines
+        languageSpec <- Text.stripPrefix "```" spec
         let (snippet, rest) = breakSnippet afterHeader
-        Just $ (lineNo, languageSpec, map snd snippet) : splitSnippets
-            (take 1 rest)
+        pure $
+            (lineNo, languageSpec, map snd snippet)
+            : splitSnippets (take 1 rest)
       where
         breakSnippet = break $ \(_lineNo, line) -> "```" `Text.isPrefixOf` line
 
     parseSnippet (lineNo, spec, content) = Snippet
-        { config    = decodeConfig configText
+        { config    =
+            fromMaybe defaultSnippetConfig
+            $ either (error . ([i|#{file}, line #{lineNo}: |] <>)) identity
+            $ Yaml.decodeEither
+            $ encodeUtf8 configText
         , content   = Text.unlines content
         , filepath  = toS $ encodeString file
-        , language
+        , language  = case lang of
+            ""  -> NoLanguage
+            "c" -> C
+            _   -> error [i|#{file}, line #{lineNo}: unknown language #{lang}|]
         , startLine = lineNo
         }
       where
         (lang, configText) = Text.break isSpace spec
-        language           = case lang of
-            ""  -> NoLanguage
-            "c" -> C
-            _   -> error [i|#{file}, line #{lineNo}: unknown language #{lang}|]
-        decodeConfig =
-            fromMaybe defaultSnippetConfig
-                . fromRight (error . ([i|#{file}, line #{lineNo}: |] <>))
-                . Yaml.decodeEither
-                . encodeUtf8
-        fromRight a = either a identity
 
 checkSnippet :: Snippet -> IO ()
-checkSnippet Snippet { language = NoLanguage } = pure ()
-checkSnippet snippet@Snippet { language = C }  = sh $ do
-    tmp <- mktempdir "" "ComputerScience.test"
-    liftIO $ do
-        let snippetFile = tmp </> "snippet.c"
-        writeTextFile snippetFile
-            $  Text.unlines
-            $  [ [i|#include "#{inc}"|] | inc <- include ]
-            <> [[i|#line #{startLine} "#{filepath}"|], content, after]
-        sourceDir <- pwd
-        procs
-            "gcc"
-            [ "-c"
-            , "-I" <> toS (encodeString sourceDir)
-            , "-Wall"
-            , "-Werror"
-            , "-Wextra"
-            , "-pedantic"
-            , toS $ encodeString snippetFile
-            , "-o" <> toS (encodeString $ snippetFile `replaceExtension` "o")
-            ]
-            empty
+checkSnippet Snippet{language = NoLanguage} = pure ()
+checkSnippet snippet@Snippet{language = C} = sh $ do
+    sourceDir <- pwd
+    mktempdir "" "ComputerScience.test" >>= pushd
+    liftIO . writeTextFile snippetFile . Text.unlines $
+        [ [i|#include "#{inc}"|] | inc <- include ]
+        <> [[i|#line #{startLine} "#{filepath}"|], content, after]
+    compile sourceDir
   where
-    Snippet { config, content, filepath, startLine } = snippet
-    SnippetConfig { after, include }                 = config
+    snippetFile = "snippet.c"
+    Snippet{config, content, filepath, startLine} = snippet
+    SnippetConfig{after, include} = config
+    compile includeDir = procs
+        "gcc"
+        (gccOptions <> ["-c", "-I" <> pathToS includeDir, pathToS snippetFile])
+        empty
+    gccOptions = ["-Wall", "-Werror", "-Wextra", "-pedantic"]
+
+pathToS :: FilePath -> Text
+pathToS = toS . encodeString
